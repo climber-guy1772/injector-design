98aca5def91a68d580f71a1fb63322138f61e175
Liam Schenk <61298858+climber-guy1772@users.noreply.github.com>
2021-11-18 00:05:53 -0500

    Update qexch.m
    Updated qexch.m to run given inputs from another file. Reference coolingmod.m to understand running qexch.m. Moving forward, we will compile our big thermal modeling functions into coolingmod.m.

--- functions/heat-transfer/qexch.m
+++ functions/heat-transfer/qexch.m
@@ -1,12 +1,22 @@
-function qexch()
+function qexch(values,header)
+
 % qexch (heat exchange) takes an array of inputs in the form:
-% {initT,runtime,TCval,MDval,SHval}
-%   initT:    inital temperature
-%   runtime:  runtime (burntime for us) of system
+% {ITval,RTval,TCval,MDval,SHval}
+% Where:
+%   ITval:    inital temperature
+%   RTval:    runtime (burntime for us) of system
 %   TCval:    thermal conductivity of material
 %   MDval:    mass density of material
 %   SHval:    specific heat of material
 
+%---Parse "values" for physical information---%
+ITval = values(1);
+RTval = values(2);
+TCval = values(3);
+MDval = values(4);
+SHval = values(5);
+
+%---Obtain geometry for model---%
 sface = -1;
 fileID = -1;
 errmsg = '';
@@ -16,9 +26,12 @@ while fileID < 0
    [fileID,errmsg] = fopen(filename);
 end
 
-% Set parameter to transient or steady state
-% For any heat transfer problems involving mass density and thermal
-% conductivity, use a transient state model
+% Set model as:      [STEADY] or [TRANSIENT]
+% Which use params:     ITval          ITval
+%                       RTval          RTval
+%                                      MDval
+%                                      TCval
+%                                      SHval         
 
 clf
 model = createpde('thermal','transient');
@@ -30,60 +43,61 @@ pdegplot(model,'FaceLabels','on')
 title('Model Geometry')
 print(['injecGeo-',date],'-dpng')
 
-% Define initial conditions for model
+%---Define initial conditions for model---%
 generateMesh(model);
 % pdemesh(model)    -   Optional; displays mesh on plot
-initCon = thermalIC(model,initT); %#ok<*NASGU> 
-size = runtime / 100;
-tlist = 0:size:runtime;
+initCon = thermalIC(model,ITval); %#ok<*NASGU> 
+size = RTval / 100;
+tlist = 0:size:RTval;
 
 tP = thermalProperties(model,'ThermalConductivity',TCval,'MassDensity',MDval,'SpecificHeat',SHval);
 
 numT = input('No. of faces w/ distinctive properties: ');
 arr = 0:1:(numT-1);
 qn = 0:1:(numT-1);
-counter = 1;
+lcv = 1;
 
-% Assign specific faces thermal properties
+%---Assign desired faces thermal properties---%
 while numT > 0
     while sface < 0
         disp(errmsg);
         sface = input('Face no.: ');
     end
+    %---Chose one of the boundary condition options & define properties---%
     disp('Boundary condition options:'); disp('1. Temperature BC'); disp('2. Heat Flux BC'); disp('3. Convection BC')
     bcselect = input('Enter boundary condition type: ');
     if bcselect == 1
         tmeas = input('Measured temp: ');
         thermalBC(model,'Face',sface,'Temperature',tmeas);
-        arr(counter) = sface;
-        counter = counter + 1;
+        arr(lcv) = sface;
+        lcv = lcv + 1;
         sface = -1;
     elseif bcselect == 2
         hfmeas = input('Heat flux: ');
         thermalBC(model,'Face',sface,'HeatFlux',hfmeas);
-        arr(counter) = sface;
-        counter = counter + 1;
+        arr(lcv) = sface;
+        lcv = lcv + 1;
         sface = -1;
     elseif bcselect == 3
         convc = input('Convection coefficient: ');
         tamb = input('Ambient temp: ');
         thermalBC(model,'Face',sface,'ConvectionCoefficient',convc,'AmbientTemperature',tamb);
-        arr(counter) = sface;
-        counter = counter + 1;
+        arr(lcv) = sface;
+        lcv = lcv + 1;
         sface = -1;
     end
     numT = numT - 1;
 end
 
-% Solve model; execution times taken to test efficiency
+%---Solve model; execution times taken to test efficiency---%
 time1 = datetime('now');
-% Assign solved model to time solution array; allows graphing
+%---Assign solved model to time solution array; allows graphing---%
 result = solve(model,tlist);
 sol = result.Temperature;
 time2 = datetime('now');
 
 i = 1;
-timeSize = runtime / 4;
+timeSize = RTval / 4;
 while i <= 4
     subplot(2,2,i)
     interval = i * timeSize;
@@ -97,12 +111,13 @@ end
 print(['thermGeo-',date],'-dpng')
 clf
 
+%---Begin output file qtOut.txt---%
 delete("qtOut.txt")
 diary("qtOut.txt")
 diary on
+disp(header)
 disp('Heat Exchange Model Outputs')
-disp(datetime('now'))
-disp('Runtime: ')
+disp('Elapsed runtime: ')
 disp(time2-time1)
 disp(['Geometry for "' filename '":'])
 str = string(evalc('feature(''hotlinks'',''off'');disp(model)'));
@@ -116,33 +131,33 @@ disp('Physical Properties:')
 str = string(evalc('feature(''hotlinks'',''off'');disp(tP)'));
 disp(str)
 
-% Convert entered boundary conditions to text output
+%---Convert entered boundary conditions to text output---%
 disp('Boundary Conditions:')
-while counter > 1
-    counter = counter - 1;
-    sface = arr(counter);
+while lcv > 1
+    lcv = lcv - 1;
+    sface = arr(lcv);
     tbc = findThermalBC(model.BoundaryConditions,'Face',sface);
     str = string(evalc('feature(''hotlinks'',''off'');disp(tbc)'));
     disp(str)
 end
 
-% Produce relevant charts and data
+%---Produce relevant charts and data---%
 hold on
-while counter <= numF
-    qn = evaluateHeatRate(result,'Face',counter);
-    disp(['Qn for F',num2str(counter),' on 0:',num2str(size),':',num2str(runtime)])
+while lcv <= numF
+    qn = evaluateHeatRate(result,'Face',lcv);
+    disp(['Qn for F',num2str(lcv),' on 0:',num2str(size),':',num2str(RTval)])
     table1 = table(tlist.',qn,'VariableNames',{'Time','Heat Flow Rate'});
     str = string(evalc('feature(''hotlinks'',''off'');disp(table1)'));
     disp(str)
     plot(tlist,qn)
-    if counter == numF
+    if lcv == numF
         Legend = cell(numF,1);
         for iter=1:numF
             Legend{iter}=strcat('Face ',num2str(iter));
         end
         legend(string(Legend))
     end
-    counter = counter + 1;
+    lcv = lcv + 1;
 end
 hold off
 title('Heat Flow Rate for Burn Duration')
