0d0928c7dd01af87011265d9f914bbbfa941434c
Liam Schenk <61298858+climber-guy1772@users.noreply.github.com>
2021-12-05 22:47:29 -0500

    Add files via upload

--- functions/flowgeo.m
+++ functions/flowgeo.m
@@ -3,31 +3,53 @@
 %          both close and open-type coaxial swirl injectors. This file will
 %          output graphical representations of spray patterns.
 
-deltaP = input('Enter change in pressure [Pa]: ');
-numInlet = input('Enter number of inlet holes: ');
-rho = input('Enter density of fluid [kg/m^3]: ');
+% Inner injector inputs
+deltaP1 = input('Enter change in pressure [Pa] for propellant: ');
+numInlet1 = input('Enter number of inlet holes for propellant: ');
+rho1 = input('Enter density of fluid [kg/m^3] for propellant: ');
 
-% Given values:
-rInlet = 0.001;     % Radius of inlet holes (m)
-rV = 0.005;         % Radius of vortex chamber (m)
-rN = rV;            % Radius of vortex chamber at base (m)
-rIn = rV-rInlet;    % Swirl arm (m)
-viscosity = 10^-6;  % Kinematic viscosity of water (m^2/s)
-nozCo = rIn/rN;     % Coefficient of nozzle opening; <1 for open end injectors
+% Outer injector inputs
+deltaP2 = input('Enter change in pressure [Pa] for oxidizer: ');
+numInlet2 = input('Enter number of inlet holes for oxidizer: ');
+rho2 = input('Enter density of fluid [kg/m^3] for oxidizer: ');
 
-if nozCo<1
-    disp('Open-type injector geometries:')
-    % Calculate the spray cone half angle for an open-type injector:
-    A = (rIn*rV)/(numInlet*rInlet^2);                  % Geometric characteristic constant
-    disCo = 0.432/(A^0.64);                            % Discharge coefficient of injector
-    mDot = disCo*pi*rN^2*(2*rho*deltaP)^0.5;           % Mass flow rate
-    Q = mDot/rho;                                      % Volume flow rate (m^3/s)
-    Re = (2*Q)/(pi*sqrt(numInlet)*rInlet*viscosity);   % Reynolds number
-    theta = atand(0.033*(A^0.338)*(Re^0.249));         % Spray cone half angle (deg)
-    lengthB = 2*3935*rN*(A^-0.621)*(Re^-0.465);        % Breakup length (m)
+% Given geometry:
+%       Inner Injector:
+%       Closed type coaxial swirl
+% Ryan/Jacob math here
 
-    % Plot and output spray geometries for open-type injector:
-    disp(['mDot: ',num2str(mDot),' [g/s]'])
-    disp(['Spray cone half angle: ',num2str(theta),' [deg]'])
-    disp(['Breakup length: ',num2str(lengthB),' [m]'])
-end
+%       Outer Injector:
+%       Open type coaxial swirl
+rInlet2 = 0.001;     % Radius of inlet holes (m)
+rV2 = 0.005;         % Radius of vortex chamber (m)
+rN2 = rV2;            % Radius of vortex chamber at base (m)
+rIn2 = rV2-rInlet2;    % Swirl arm (m)
+viscosity2 = 10^-6;  % Kinematic viscosity of water (m^2/s)
+%nozCo = rIn/rN;      Coefficient of nozzle opening; <1 for open end injectors
+
+disp('Open-type injector geometries:')
+% Calculate the spray cone half angle for an open-type injector:
+A2 = (rIn2*rV2)/(numInlet2*rInlet2^2);                   % Geometric characteristic constant
+disCo2 = 0.432/(A2^0.64);                                % Discharge coefficient of injector
+mDot2 = disCo2*pi*rN2^2*(2*rho2*deltaP2)^0.5;            % Mass flow rate
+Q2 = mDot2/rho2;                                         % Volume flow rate (m^3/s)
+Re2 = (2*Q2)/(pi*sqrt(numInlet2)*rInlet2*viscosity2);    % Reynolds number
+theta2 = atand(0.033*(A2^0.338)*(Re2^0.249));            % Spray cone half angle (deg)
+lengthB2 = 2*3935*rN2*(A2^-0.621)*(Re2^-0.465);          % Breakup length (m)
+
+% Plot and output spray geometries for open-type injector:
+disp(['mDot: ',num2str(mDot2),' [g/s]'])
+disp(['Spray cone half angle: ',num2str(theta2),' [deg]'])
+disp(['Breakup length: ',num2str(lengthB2),' [m]'])
+
+% Here's some math for impingement of two sheets
+% Stuff we need:
+%       - Filling coefficient
+%       - mdot1
+%     y - mdot2
+%       - axial and circumferential (tangential?) velocity for each cone
+%     y - density for oxidizer and fuel
+%
+% Questions to answer:
+%       - How can we achieve a recess number near 1
+
